10018d75372f148bf9c7e3a5a328537c3876e5cd
aoyagih <hiroki3290@gmail.com>
2020-08-12 02:07:49 +0900

    Initial Commit

---
+++ DeepLinkApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeepLinkApp
+//
+//  Created by Aoyagi Hiroki on 2020/08/12.
+//  Copyright © 2020 aoyagih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
